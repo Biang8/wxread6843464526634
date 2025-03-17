1338d0b2f7956b6eaa8ce1819214eabaa91ce4d2
Biang0 <109459544+Biang0@users.noreply.github.com>
2025-03-17 14:22:49 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import hashlib
 import requests
 import urllib.parse
 from push import push
-from config import data, headers, cookies, READ_NUM, PUSH_METHOD, book_mapping
+from config import data, headers, cookies, READ_NUM, PUSH_METHOD
 
 # 配置日志格式
 logger = logging.getLogger(__name__)
@@ -20,13 +20,11 @@ COOKIE_DATA = {"rq": "%2Fweb%2Fbook%2Fread"}
 READ_URL = "https://weread.qq.com/web/book/read"
 RENEW_URL = "https://weread.qq.com/web/login/renewal"
 
-# 获取当前阅读书籍名称
-current_book = book_mapping.get(data["b"], "未知书籍")
-logging.info(f"📖 当前阅读书籍: {current_book}")
 
 def encode_data(data):
     """数据编码"""
     return '&'.join(f"{k}={urllib.parse.quote(str(data[k]), safe='')}" for k in sorted(data.keys()))
+
 
 def cal_hash(input_string):
     """计算哈希值"""
@@ -42,6 +40,7 @@ def cal_hash(input_string):
 
     return hex(_7032f5 + _cc1055)[2:].lower()
 
+
 def get_wr_skey():
     """刷新cookie密钥"""
     response = requests.post(RENEW_URL, headers=headers, cookies=cookies,
@@ -51,6 +50,7 @@ def get_wr_skey():
             return cookie.split('=')[-1][:8]
     return None
 
+
 index = 1
 while index <= READ_NUM:
     data['ct'] = int(time.time())
@@ -59,14 +59,14 @@ while index <= READ_NUM:
     data['sg'] = hashlib.sha256(f"{data['ts']}{data['rn']}{KEY}".encode()).hexdigest()
     data['s'] = cal_hash(encode_data(data))
 
-    logging.info(f"⏱️ 正在阅读《{current_book}》，第 {index} 次...")
+    logging.info(f"⏱️ 尝试第 {index} 次阅读...")
     response = requests.post(READ_URL, headers=headers, cookies=cookies, data=json.dumps(data, separators=(',', ':')))
     resData = response.json()
 
     if 'succ' in resData:
         index += 1
         time.sleep(30)
-        logging.info(f"✅ 阅读成功，累计阅读时长：{(index - 1) * 0.5} 分钟")
+        logging.info(f"✅ 阅读成功，阅读进度：{(index - 1) * 0.5} 分钟")
 
     else:
         logging.warning("❌ cookie 已过期，尝试刷新...")
@@ -76,14 +76,14 @@ while index <= READ_NUM:
             logging.info(f"✅ 密钥刷新成功，新密钥：{new_skey}")
             logging.info(f"🔄 重新本次阅读。")
         else:
-            ERROR_CODE = "❌ 无法获取新密钥或者 WXREAD_CURL_BASH 配置有误，终止运行。"
+            ERROR_CODE = "❌ 无法获取新密钥或者WXREAD_CURL_BASH配置有误，终止运行。"
             logging.error(ERROR_CODE)
             push(ERROR_CODE, PUSH_METHOD)
             raise Exception(ERROR_CODE)
     data.pop('s')
 
-logging.info(f"🎉 阅读完成！书籍：《{current_book}》")
+logging.info("🎉 阅读脚本已完成！")
 
 if PUSH_METHOD not in (None, ''):
     logging.info("⏱️ 开始推送...")
-    push(f"🎉 微信读书自动阅读完成！\n📖 书籍：《{current_book}》\n⏱️ 阅读时长：{(index - 1) * 0.5}分钟。", PUSH_METHOD)
+    push(f"🎉 微信读书自动阅读完成！\n⏱️ 阅读时长：{(index - 1) * 0.5}分钟。", PUSH_METHOD)
